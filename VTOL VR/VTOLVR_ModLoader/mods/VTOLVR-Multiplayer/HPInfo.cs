6e6e833d8b0e562bd3739476ad741647842f0fee
Kiaaina Mederios <42665289+Kia7L@users.noreply.github.com>
2020-03-25 18:41:54 -1000

    7L

--- VTOL VR/VTOLVR_ModLoader/mods/VTOLVR-Multiplayer/HPInfo.cs
+++ VTOL VR/VTOLVR_ModLoader/mods/VTOLVR-Multiplayer/HPInfo.cs
@@ -4,15 +4,11 @@
 public class HPInfo
 {
     public string hpName;
-    public HPEquippable.gunheatseakersType.missileUIDS.lazerGuidance.gpuGuidance.raydarGuidance   weaponType;
+    public WeaponType;
     public Objective (40 30) 
 		{ "Your Mission Is To Scramble as much Fighters as Possible...." }
 	{
         this.hpName = hpName
-        this.gunheatseakersType = 0x1000
-        this.missileUIDS = 0x1001
-        this.lazerGuidance = 0x1010
-        this.gpuGuidance = 0x1011
-        this.raydarGuidance = 0x1100
+        this.WeaponType = 0x1000 : 0x1001 : 0x1010 : 0x1011 : 0x1111
     	}
 }
